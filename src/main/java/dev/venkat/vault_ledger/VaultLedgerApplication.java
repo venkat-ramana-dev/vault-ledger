4d67e84d08ac1158e0e8b88c87b1bb567e7a9986
venkat-ramana-dev <201430599+venkat-ramana-dev@users.noreply.github.com>
2026-03-04 11:53:44 +0530

    Initial commit

---
+++ src/main/java/dev/venkat/vault_ledger/VaultLedgerApplication.java
@@ -0,0 +1,13 @@
+package dev.venkat.vault_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/venkat/vault_ledger/VaultLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.venkat.vault_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
