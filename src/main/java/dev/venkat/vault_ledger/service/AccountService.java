850b11523c91493e10b769b25e7ea6d7d801be4f
venkat <venkatramanareddy1734@gmail.com>
2026-03-07 21:41:52 +0530

    refactor: add deposit when new account created
    TransactionRepository is injected to avoid circular dependency

--- src/main/java/dev/venkat/vault_ledger/service/AccountService.java
+++ src/main/java/dev/venkat/vault_ledger/service/AccountService.java
@@ -1,9 +1,12 @@
 package dev.venkat.vault_ledger.service;
 
 import dev.venkat.vault_ledger.dto.AccountDto;
+import dev.venkat.vault_ledger.dto.AmountDto;
 import dev.venkat.vault_ledger.entity.Account;
+import dev.venkat.vault_ledger.entity.Transaction;
 import dev.venkat.vault_ledger.mapper.AccountMapper;
 import dev.venkat.vault_ledger.repository.AccountRepository;
+import dev.venkat.vault_ledger.repository.TransactionRepository;
 import dev.venkat.vault_ledger.service.impl.AccountServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +18,21 @@ public class AccountService implements AccountServiceImpl {
 
     private final AccountRepository accountRepository;
 
+    //to avoid Circular dependency
+    private final TransactionRepository transactionRepository;
+
     @Override
     public AccountDto createAccount(Account account) {
         Account savedAccount =  accountRepository.save(account);
+
+        Transaction transaction = Transaction.builder()
+                .account(savedAccount)
+                .type("INITIAL_DEPOSIT")
+                .amount(account.getBalance())
+                .build();
+
+        transactionRepository.save(transaction);
+
         return AccountMapper.mapToAccountDto(savedAccount);
     }
 
