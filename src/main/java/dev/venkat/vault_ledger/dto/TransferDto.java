770cf0c68ede2da6ad235ec836affb2cb084d7f3
venkat <venkatramanareddy1734@gmail.com>
2026-03-09 14:29:17 +0530

    feat: implement transfer RESI API
    Amount is tranfered from fromId to toId

--- src/main/java/dev/venkat/vault_ledger/dto/TransferDto.java
+++ src/main/java/dev/venkat/vault_ledger/dto/TransferDto.java
@@ -1,5 +1,7 @@
 package dev.venkat.vault_ledger.dto;
 
+import java.math.BigDecimal;
+
 public record TransferDto(Long toId,
-                          AmountDto amountDto) {
+                          BigDecimal amount) {
 }
