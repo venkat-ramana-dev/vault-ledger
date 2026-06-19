0700b149ef44b8c07a2a58a3edddd5b7b4744948
venkat <venkatramanareddy1734@gmail.com>
2026-06-19 19:08:10 +0530

    refactor : Account entity for append-only ledger

--- src/main/java/dev/venkat/vault_ledger/entity/Account.java
+++ src/main/java/dev/venkat/vault_ledger/entity/Account.java
@@ -3,8 +3,10 @@ package dev.venkat.vault_ledger.entity;
 import dev.venkat.vault_ledger.enums.AccountStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "accounts")
@@ -24,11 +26,12 @@ public class Account {
     @Column(name = "account_holder_name", nullable = false)
     private String accountHolderName;
 
-    @Column(nullable = false)
-    private BigDecimal balance;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AccountStatus accountStatus = AccountStatus.ACTIVE;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
 }
