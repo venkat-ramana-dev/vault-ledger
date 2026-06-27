b2ee08f83f8934db0b510b82386fa06ecdf0267e
venkat <venkatramanareddy1734@gmail.com>
2026-06-27 10:22:08 +0530

    feat: add Flyway support, initial schema migration

--- src/main/java/dev/venkat/vault_ledger/VaultLedgerApplication.java
+++ src/main/java/dev/venkat/vault_ledger/VaultLedgerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VaultLedgerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VaultLedgerApplication.class, args);
+
+        SpringApplication.run(VaultLedgerApplication.class, args);
 	}
 
 }
